13485225916dd6a94084cbb1524ec0378efcc040
Angel Torres <jatorresdev@gmail.com>
2017-03-08 10:14:18 -0500

    Se añaden nuevos test

--- Contacts.UITests/Tests.cs
+++ Contacts.UITests/Tests.cs
@@ -46,5 +46,36 @@ namespace Contacts.UITests
 			var profileElement = app.Query(x => x.Marked("Perfil")); 
 			Assert.IsTrue(profileElement.Any());
 		}
+
+		[Test]
+		public void LoginWillFail()
+		{
+			app.EnterText(x => x.Marked("entPassword"), "Password67");
+			app.Screenshot("Se introdujo un password: Password67");
+			app.Tap(x => x.Marked("btnLogin"));
+			app.Screenshot("Se presiono el inicio");
+			app.WaitForNoElement("indIsBusy", "No aparecio error IsBusy");
+			var errorDialog = app.Query("Error");
+			Assert.IsTrue(errorDialog.Any(), "No se mostro el error");
+		}
+
+		[Test]
+		public void CellWillBePressed()
+		{
+			LoginWillSuccess();
+			app.Tap(x => x.Marked("entBirthday"));
+			app.Screenshot("Tapped on view with class: EntryEditText marked: entBirthday");
+			app.EnterText(x => x.Marked("entBirthday"), "12/12/1990");
+			app.Tap(x => x.Marked("btnSaveProfile"));
+			app.Screenshot("Se guardo el perfil");
+			app.WaitForNoElement("indIsBusy", "No aparecio error IsBusy");
+			if (platform == Platform.Android)
+				app.TouchAndHold(x => x.Index(4));
+			else
+				app.SwipeRightToLeft(x => x.Index(4));
+			app.Screenshot("Se hizo un long press en una celda");
+			var deleteOption = app.Query("Eliminar");
+			Assert.GreaterOrEqual(deleteOption.Count(), 1, "No hay opcion de eliminar");
+		}
 	}
 }
